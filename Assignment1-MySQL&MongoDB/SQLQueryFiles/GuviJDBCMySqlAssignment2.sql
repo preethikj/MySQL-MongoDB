16cdc8edef950eeeb575160027086857d4fc2168
preethikj <46474339+preethikj@users.noreply.github.com>
2026-04-20 23:48:12 +0530

    Delete Assignment1-MySQL&MongoDB directory

--- Assignment1-MySQL&MongoDB/SQLQueryFiles/GuviJDBCMySqlAssignment2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE employee_jdbc(empcode INT PRIMARY KEY,
-							empname VARCHAR(100),
-                            empage INT,
-                            salary DECIMAL(7,2));
-                            
-SELECT * FROM employee_jdbc;
-
